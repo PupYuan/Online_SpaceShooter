dcf8b980317cd90f0ea1163f5ff13cdd7572907e
DengJunYuan <540997806@qq.com>
2019-01-06 09:47:38 +0800

    通过锁住command_list防止出现竞争条件

--- Server_DeterministicLock/Serv/Logic/handlePlayerMsg.cs
+++ Server_DeterministicLock/Serv/Logic/handlePlayerMsg.cs
@@ -33,30 +33,34 @@ public partial class HandlePlayerMsg
         Command cmd = new Command();
         cmd.input.x = x;
         cmd.input.y = y;
-        //使用Enqueue的话，我们假定了接收到的包都是顺序的，并且不会丢包；若出现这种情况怎么办？
-        ServNet.instance.command_list[player.id].Enqueue(cmd);
-        //在这里判断是否收到了所有玩家下一关键帧的控制信息
-        foreach(var item in ServNet.instance.command_list)
+        //多线程处理消息，此时需要防止竞争
+        lock (ServNet.instance.command_list)
         {
-            //有玩家的关键帧还未送达
-            if (item.Value.Count == 0)
-                return;
-        }
-        //所有玩家的关键帧都送达，所有玩家的控制消息队列都出列一项，并广播给所有客户
-		//广播
-		ProtocolBytes protocolRet = new ProtocolBytes();
-		protocolRet.AddString ("SyncCommand");
-        protocolRet.AddUint(KeyFrameNumber+5);//K1
-        protocolRet.AddUint(KeyFrameNumber+10);//K2
-        protocolRet.AddInt(ServNet.instance.command_list.Count);
-        foreach (var item in ServNet.instance.command_list)
-        {
-            protocolRet.AddString(item.Key);
+            //使用Enqueue的话，我们假定了接收到的包都是顺序的，并且不会丢包；若出现这种情况怎么办？
+            ServNet.instance.command_list[player.id].Enqueue(cmd);
+            //在这里判断是否收到了所有玩家下一关键帧的控制信息
+            foreach (var item in ServNet.instance.command_list)
+            {
+                //有玩家的关键帧还未送达
+                if (item.Value.Count == 0)
+                    return;
+            }
+            //所有玩家的关键帧都送达，所有玩家的控制消息队列都出列一项，并广播给所有客户
+            //广播
+            ProtocolBytes protocolRet = new ProtocolBytes();
+            protocolRet.AddString("SyncCommand");
+            protocolRet.AddUint(KeyFrameNumber + 5);//K1
+            protocolRet.AddUint(KeyFrameNumber + 10);//K2
+            protocolRet.AddInt(ServNet.instance.command_list.Count);
+            foreach (var item in ServNet.instance.command_list)
+            {
+                protocolRet.AddString(item.Key);
 
-            Command _cmd = item.Value.Dequeue();
-            protocolRet.AddFloat(_cmd.input.x);
-            protocolRet.AddFloat(_cmd.input.y);
+                Command _cmd = item.Value.Dequeue();
+                protocolRet.AddFloat(_cmd.input.x);
+                protocolRet.AddFloat(_cmd.input.y);
+            }
+            ServNet.instance.Broadcast(protocolRet);
         }
-        ServNet.instance.Broadcast (protocolRet);
 	}
 }
