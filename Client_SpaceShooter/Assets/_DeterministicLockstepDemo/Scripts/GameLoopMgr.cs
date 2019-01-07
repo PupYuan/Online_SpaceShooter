5e23bd9945ab6b7178ed3e3af04b2dc4e635b177
DengJunYuan <540997806@qq.com>
2019-01-07 16:40:52 +0800

    由GameLoopMgr统一tick游戏逻辑

--- Client_SpaceShooter/Assets/_DeterministicLockstepDemo/Scripts/GameLoopMgr.cs
+++ Client_SpaceShooter/Assets/_DeterministicLockstepDemo/Scripts/GameLoopMgr.cs
@@ -8,18 +8,86 @@ namespace DeterministicLockstepDemo
     public class GameLoopMgr : MonoBehaviour
     {
         public static GameLoopMgr instance;
-        public uint updateK1 = 0;
-        public uint updateK2 = 0;
-        private void Awake()
-        {
-            instance = this;
-        }
         public GameObject[] PlayerPrefab;
         //通过id来更新
         public Dictionary<string, PlayerController> m_playerControllerList = new Dictionary<string, PlayerController>();
-        public Dictionary<string,Command> command_list = new Dictionary<string, Command>();
+        public Dictionary<string, Command> command_list = new Dictionary<string, Command>();
+
+        public uint updateK1 = 0;
+        public uint updateK2 = 0;
+        private uint sequence = 0;//当前帧号
+        public uint KeyFrameInterval = 5;//固定法：关键帧之前相差固定帧数
+        public uint KeyFrameNumber = 0;//下一关键帧的帧号
+        private void Awake()
+        {
+            instance = this;
+            
+        }
+        //逻辑循环，目前直接使用FixUpdate
+        private void FixedUpdate()
+        {
+            Simulate();
+        }
+
+        public void Simulate()
+        {
+            //若当前是关键帧
+            if (sequence == KeyFrameNumber)
+            {
+                //检查是否有K1的UPDATE数据
+                if (command_list.Count <= 0)
+                {
+                    Debug.Log("LockStep");
+                    return;//LockStep
+                }
+                else
+                {
+                    KeyFrameNumber = GameLoopMgr.instance.updateK2;
+                    SendCommand();
+                }
+            }
+            else//若当前处于模拟帧（即输入采取上一次关键帧时的输入I进行模拟）
+            {
+                //遍历所有玩家的controller，并让他们执行指令
+                foreach(var player in m_playerControllerList)
+                {
+                    if (command_list.ContainsKey(player.Key))
+                        player.Value.ExecuteCommand(command_list[player.Key]);
+                    else Debug.Log("No " + player.Key +" ' s Command");
+                }
+                sequence++;
+            }
+        }
+
+        //和原本直接采用Input.GetKey得到的向量改变速度相比，我们采取离散的
+        public CommandInput CollectCommandInput()
+        {
+            CommandInput input = new CommandInput();
+            input.x = (Fix64)Input.GetAxis("Horizontal");
+            input.z = (Fix64)Input.GetAxis("Vertical");
+            return input;
+        }
+
+        public void SendCommand()
+        {
+            Command cmd = new Command();
+            cmd.input = CollectCommandInput();      // 获取指令
+            cmd.sequence = KeyFrameNumber;
+
+            ProtocolBytes proto = new ProtocolBytes();
+            proto.AddString("SyncCommand");
+            proto.AddUint(cmd.sequence);
+            proto.AddFix(cmd.input.x);
+            proto.AddFix(cmd.input.z);
+
+            NetMgr.srvConn.Send(proto);
+        }
+
+
         private void Start()
         {
+            SendCommand();//发送最开始的关键帧，帧号为0
+            KeyFrameNumber += KeyFrameInterval;//开始五帧的模拟为空
             int Count = 0;//生成玩家时给予一个偏移量
             foreach (var player in GameMgr.instance.player_list)
             {
--- Client_SpaceShooter/Assets/_DeterministicLockstepDemo/Scripts/PlayerController.cs
+++ Client_SpaceShooter/Assets/_DeterministicLockstepDemo/Scripts/PlayerController.cs
@@ -13,76 +13,17 @@ namespace DeterministicLockstepDemo
     {
         [HideInInspector]
         public string player_id;
-        private uint sequence = 0;//当前帧号
-        public uint KeyFrameInterval = 5;//固定法：关键帧之前相差固定帧数
-        public uint KeyFrameNumber = 0;//下一关键帧的帧号
+        
+        
         public Fix64 movingSpeed = (Fix64)8;
         public CtrlType ctrlType = CtrlType.player;
         public FixVector3 logicPosition = FixVector3.Zero;//逻辑位置，用于实际碰撞判断以及网络同步
 
-        //发送最开始的关键帧，此时关键帧号为0
-        private void Start()
+        private void Awake()
         {
-            SendCommand();//发送最开始的关键帧，帧号为0
-            KeyFrameNumber += KeyFrameInterval;//开始五帧的模拟为空
             logicPosition.x = (Fix64)transform.position.x;
             logicPosition.y = (Fix64)transform.position.y;
             logicPosition.z = (Fix64)transform.position.z;
-        }
-
-        private void FixedUpdate()
-        {
-            Simulate();
-        }
-        //和原本直接采用Input.GetKey得到的向量改变速度相比，我们采取离散的
-        public CommandInput CollectCommandInput()
-        {
-            CommandInput input = new CommandInput();
-            input.x = (Fix64)Input.GetAxis("Horizontal");
-            input.z = (Fix64)Input.GetAxis("Vertical");
-            return input;
-        }
-
-        public void Simulate()
-        {
-            //若当前是关键帧
-            if(sequence == KeyFrameNumber)
-            {
-                //检查是否有K1的UPDATE数据
-                if (GameLoopMgr.instance.command_list.Count <= 0)
-                {
-                    return;//LockStep
-                }
-                else
-                {
-                    KeyFrameNumber = GameLoopMgr.instance.updateK2;
-                    if(ctrlType == CtrlType.player)
-                        SendCommand();
-                }
-            }
-            else//若当前处于模拟帧（即输入采取上一次关键帧时的输入I进行模拟）
-            {
-                if (GameLoopMgr.instance.command_list.ContainsKey(player_id))
-                    ExecuteCommand(GameLoopMgr.instance.command_list[player_id]);
-                else Debug.Log("No Command");
-                sequence++;
-            }
-            
-        }
-
-        public void SendCommand()
-        {
-            Command cmd = new Command();
-            cmd.input = CollectCommandInput();      // 获取指令
-            cmd.sequence = KeyFrameNumber;
-
-            ProtocolBytes proto = new ProtocolBytes();
-            proto.AddString("SyncCommand");
-            proto.AddUint(cmd.sequence);
-            proto.AddFix(cmd.input.x);
-            proto.AddFix(cmd.input.z);
-
-            NetMgr.srvConn.Send(proto);
         }
 
         public void ExecuteCommand(Command command)
